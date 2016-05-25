13ddafb27a1341c7920cbf87621a40ef958a6762
Stanislas Cuenat <stanislas.cuenat@epitech.eu>
2016-05-25 11:54:32 +0200

    adding signal gestion

---
+++ src/signal.c
@@ -0,0 +1,11 @@
+/*
+** signal.c for  in /home/stanislas/clone/PSU_2015_42sh
+**
+** Made by CUENAT
+** Login   <stanislas@epitech.net>
+**
+** Started on  Tue May 24 17:09:20 2016 CUENAT
+** Last update Tue May 24 17:09:42 2016 CUENAT
+*/
+
+int	cltrc(sig );
