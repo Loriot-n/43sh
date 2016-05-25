bd1abe53967fb25c96a950e2a144049c47ddbc74
Stanislas Cuenat <stanislas.cuenat@epitech.eu>
2016-05-25 15:36:34 +0200

    ok

--- src/build_exit.c
+++ src/build_exit.c
@@ -5,7 +5,7 @@
 ** Login   <stanislas@epitech.net>
 **
 ** Started on  Wed May 25 15:19:23 2016 CUENAT
-** Last update Wed May 25 15:25:29 2016 CUENAT
+** Last update Wed May 25 15:36:07 2016 CUENAT
 */
 
 #include "shell.h"
@@ -16,7 +16,7 @@ void	ft_exit(t_shell *shell, char *tkn, int end)
   (void)(end);
   printf("exit\n");
   if (shell->cur_instr[1] != NULL)
-    exit(my_getnbr(shell->cur_instr[1]));
+    exit(my_getnbr(shell->cur_exec[1]));
   else
     exit(0);
 }
