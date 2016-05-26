8fb2f2b8827097c58958e164c37e54a4bb26f9e1
Nicolas Loriot <nicolas.loriot@epitech.eu>
2016-05-26 17:41:40 +0200

    header arrow handling

---
+++ inc/arrows.h
@@ -0,0 +1,17 @@
+/*
+** arrows.h for 42sh in /home/nico/rendu/S02/Unix/PSU_2015_42sh/inc
+** 
+** Made by Nicolas Loriot
+** Login   <loriot_n@epitech.net>
+** 
+** Started on  Thu May 26 17:19:15 2016 Nicolas Loriot
+** Last update Thu May 26 17:41:21 2016 Nicolas Loriot
+*/
+
+#ifndef ARROWS_H_
+# define ARROWS_H_
+
+#include <ncurses.h>
+#include <stropts.h>
+
+#endif /* !ARROWS_H_  */
