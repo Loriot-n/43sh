7f6301bc9b4f4581a296a438c3b38b7390b3e6d3
Stanislas Cuenat <stanislas.cuenat@epitech.eu>
2016-06-03 15:01:46 +0200

    ok

--- src/check_input2.c
+++ src/check_input2.c
@@ -5,7 +5,7 @@
 ** Login   <maire_q@epitech.eu>
 **
 ** Started on  %cdate maire_q
-** Last update Thu Jun  2 15:04:57 2016 CUENAT
+** Last update Fri Jun  3 15:01:29 2016 CUENAT
 */
 
 #include "shell.h"
@@ -20,6 +20,9 @@ int	ft_check_input_redirect(char **tab, int i)
 	return (-1);
       else if (tab[i + 1] == NULL)
 	return (-1);
+      if (tab[i + 1] != NULL && (strchr(tab[i + 1], '|') != NULL ||
+				 strchr(tab[i + 1], '&') != NULL))
+	return (-1);
     }
   return (0);
 }
@@ -32,6 +35,9 @@ int	ft_check_redirect(char **tab, int i)
 	return (-1);
       if (tab[i + 2] != NULL &&
 	  (strcmp(tab[i + 2], ">") == 0 || strcmp(tab[i + 2], ">>") == 0))
+	return (-1);
+      if (tab[i + 1] != NULL && (strchr(tab[i + 1], '&') != NULL ||
+				 strchr(tab[i + 1], '|') != NULL))
 	return (-1);
     }
   return (0);
