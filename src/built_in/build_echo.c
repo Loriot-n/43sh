194c0c5262a8e45d80fd39695880d21f1ca2f039
qwebify <quentin.maire@epitech.eu>
2016-06-05 20:10:28 +0200

    echo -n is now working as a charm

--- src/built_in/build_echo.c
+++ src/built_in/build_echo.c
@@ -10,6 +10,18 @@
 
 #include "shell.h"
 
+void	my_putstr(char *str)
+{
+  int	i;
+
+  i = 0;
+  while(str[i])
+    {
+      write(1, &str[i], 1);
+      i++;
+    }
+}
+
 void	print_it_baby(char *line)
 {
   int	i;
@@ -17,8 +29,10 @@ void	print_it_baby(char *line)
   char	*one;
 
   tmp = 0;
+  if (!line)
+    return ;
   if (line[0] != '"' && line[0] != '\'')
-    return ((void)printf("%s", line));
+    return (my_putstr(line));
   one = NULL;
   tmp = line[0];
   i = 1;
@@ -29,13 +43,51 @@ void	print_it_baby(char *line)
   (!(one = malloc(sizeof(char) * i + 2))) ? exit(EXIT_FAILURE) : 0;
   strncpy(one, &line[1], i - 1);
   one[i - 1] = 0;
-  printf("%s", one);
+  my_putstr(one);
   print_it_baby(&line[i + 1]);
+}
+
+char	*eval_it(char *line)
+{
+  int	i;
+  int	j;
+  char	*answer;
+
+  if (!(answer = malloc(sizeof(char) * strlen(line) + 1)))
+    exit(EXIT_FAILURE);
+  i = 0;
+  j = 0;
+  while (line[i])
+    {
+      if (line[i] == '\\' && line[i + 1] == 'n')
+	{
+	  answer[j] = 10;
+	  i += 1;
+	}
+      else
+	answer[j] = line[i];
+      j++;
+      i++;
+    }
+  free(line);
+  answer[j] = 0;
+  return (answer);
+}
+
+char	*remove_carreturn(char *line, int *check)
+{
+  if (!line || strlen(line) < 4 ||
+      (line[strlen(line) - 1] != '"' && line[strlen(line) - 1]
+		!= '\'') || line[strlen(line) - 2] != 10)
+    return (line);
+  *check = 1;
+  line[strlen(line) - 2] = '"';
+  line[strlen(line) - 1] = 0;
+  return (line);
 }
 
 int	ft_echo(t_shell *shell)
 {
-  char	*cmd;
   int	i;
 
   if (shell->cur_exec[1] == NULL)
@@ -43,20 +95,17 @@ int	ft_echo(t_shell *shell)
       printf("\n");
       return (0);
     }
-  cmd = shell->cur_exec[1];
-  if (strcmp(shell->cur_exec[1], "-n") == 0)
-    {
-      cmd = strdup(shell->cur_exec[2]);
-      cmd[strlen(cmd) - 1] = 0;
-    }
-  i = 1;
+  i = (strcmp(shell->cur_exec[1], "-n") == 0) ? 2 : 1;
   while (shell->cur_exec[i])
     {
+      if (shell->cur_exec[i][0] == '\'' || shell->cur_exec[i][0] == '"')
+	shell->cur_exec[i] = eval_it(shell->cur_exec[i]);
       print_it_baby(shell->cur_exec[i]);
       if (shell->cur_exec[i + 1])
-	putchar(' ');
+	my_putstr(" ");
       i++;
     }
-  putchar(10);
+  if (strcmp(shell->cur_exec[1], "-n") != 0)
+    putchar(10);
   return (0);
 }
